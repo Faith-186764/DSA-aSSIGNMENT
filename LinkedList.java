90188f2250f151098b5dade1c74a6c4b14ba4244
Faith-186764 <Jepkemoi.cheruiyot@strathmore.edu>
2025-03-23 19:22:19 +0300

    Update Redmi with usage instractions

--- LinkedList.java
+++ LinkedList.java
@@ -1,1 +1,68 @@
+class Node{
+    int data;
+    Node next;
 
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class SinglyLinkedList{
+    Node head;
+    public SinglyLinkedList(){
+
+    }
+    //insert at the beginning
+    public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            
+        
+    }
+    //insert at the end
+    public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    //Delete at the beginning
+    public void deleteAtBeginning(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;
+    }
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+public class LinkedList {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.insertAtBeginning(10);
+        sll.insertAtBeginning(20);
+        sll.insertAtBeginning(30);
+        sll.insertAtBeginning(40);
+        sll.insertAtEnd(50);
+        System.out.println("Linked List:");
+        sll.display();
+
+        sll.deleteAtBeginning();
+        System.out.println("after deleting at the beginning:");
+        sll.display();
+    }
+}
